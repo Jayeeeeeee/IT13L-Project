d27110b25f9bddb89c39f103b44b8a744fc55eff
Jayeeeeeee <xstopoyx@gmail.com>
2023-10-02 21:58:11 +0800

    10/02/23

--- ManUI.vb
+++ ManUI.vb
@@ -2,4 +2,12 @@
     Private Sub btnLogOut_Click(sender As Object, e As EventArgs) Handles btnLogOut.Click
         Logout(Me)
     End Sub
+
+    Private Sub btnRoomProfile_Click(sender As Object, e As EventArgs) Handles btnRoomProfile.Click
+
+    End Sub
+
+    Private Sub btnDash_Click(sender As Object, e As EventArgs) Handles btnDash.Click
+
+    End Sub
 End Class

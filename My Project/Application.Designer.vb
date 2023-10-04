bc833ae0f6759a99214206a841ae6ff108706cf0
Jayeeeeeee <xstopoyx@gmail.com>
2023-10-04 23:02:42 +0800

    10/04/23

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.HotelSystem.Form1
+            Me.MainForm = Global.HotelSystem.Login
         End Sub
     End Class
 End Namespace
